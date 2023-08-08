9feaabf768efb150660e4b4e8c0e9fce76836d1e
Ram Poudel <rp164@uakron.edu>
2023-08-08 12:57:01 -0400

    Modify dashboard

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
         'med': '1470px'
       },
       colors:{
-        'whitish':'#f7f6f4'
+        'whitish':'#f7f6f4',
+        'cool-blue':'#121c4f'
+      },
+      backgroundImage:{
+        'custom-gradient':'-moz-linear-gradient(top, #121c4f 0%, #121c4f 50%, #d7d2c8 50%, #d7d2c8 100%)',
       }
     },
   },
